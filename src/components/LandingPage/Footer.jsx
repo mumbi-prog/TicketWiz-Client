ec2af8a8cc5c7f4edf30c95e2f02ac91193e201c
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-03 21:32:11 +0300

    Finished styling social links on footer

--- src/components/LandingPage/Footer.jsx
+++ src/components/LandingPage/Footer.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import logo from './../../images/logo.png';
-import {FaEnvelope} from "react-icons/fa";
-import {FaMapMarkerAlt} from "react-icons/fa";
-import {BiSolidPhone} from "react-icons/bi";
+import {FaEnvelope} from "react-icons/fa6";
+import {FaLocationDot} from "react-icons/fa6";
+import {FaPhone} from "react-icons/fa6";
+import {BiLogoFacebook} from "react-icons/bi";
+import {FaXTwitter} from "react-icons/fa6";
+import { BiLogoInstagram } from 'react-icons/bi';
+import { BiLogoTiktok } from 'react-icons/bi';
+
 import "./landingpage.css";
 
 function Footer() {
@@ -17,8 +22,11 @@ function Footer() {
                     </div>
                     <p>Connecting You to Exciting Events with Fast and Hassle-Free Ticketing, Making Every Experience Memorable, One Click at a Time.</p>
                 </div>
-                <div className="social-links">
-                    
+                <div className="social">
+                    <a href="#socials"><BiLogoFacebook/></a>
+                    <a href="#socials"><BiLogoInstagram/></a>
+                    <a href="#socials"><FaXTwitter/></a>
+                    <a href="#socials"><BiLogoTiktok/></a>
                 </div>
             </div>
             <div className="links1">
@@ -32,19 +40,19 @@ function Footer() {
                 </ul>
             </div>
             <div className="links2">
-                <h3>Quick links</h3>
+                <h3>Resources</h3>
                 <ul>
-                    <li><a href="#Featuredevents">Home</a></li>
-                    <li><a href="#Featuredevents">Featured events</a></li>
-                    <li><a href="#Featuredevents">Create event</a></li>
-                    <li><a href="#Featuredevents">Our Clients</a></li>
-                    <li><a href="#Featuredevents">Contact</a></li>
+                    <li><a href="#a">Event Planning Guides</a></li>
+                    <li><a href="#a">Ticketing FAQs</a></li>
+                    <li><a href="#a">Event Promotion Tips</a></li>
+                    <li><a href="#a">Checklists and Templates</a></li>
+                    <li><a href="#a">Local Event Resources</a></li>
                 </ul>
             </div>
             <div className="contact-details">
                 <h3>Get in Touch</h3>
-                <p><FaMapMarkerAlt className='contact-icon'/> 123 Mwia Lane, Nairobi</p>
-                <p><BiSolidPhone className='contact-icon'/> +254 719642456 | +254 728510900</p>
+                <p><FaLocationDot className='contact-icon'/> 123 Mwia Lane, Nairobi</p>
+                <p><FaPhone className='contact-icon'/> +254 719642456 | +254 728510900</p>
                 <p><FaEnvelope className='contact-icon'/> abc.email.com</p>
             </div>
         </div>
