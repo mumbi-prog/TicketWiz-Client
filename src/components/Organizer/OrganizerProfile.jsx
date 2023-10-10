3d16d93840403cc2e54dbb71e408f2bf7b7480ea
Ken <kabutukenneth3@gmail.com>
2023-10-10 09:35:17 +0300

    implement jsx with organizers info

--- src/components/Organizer/OrganizerProfile.jsx
+++ src/components/Organizer/OrganizerProfile.jsx
@@ -14,4 +14,24 @@ const OrganizerProfile = () => {
                 console.log('Error fetching organizer information')
             });
     }, []);
-}
+
+
+
+  return (
+    <div>
+      <h2>Organizer Profile</h2>
+      {organizerInfo ? (
+        <div>
+          <p>First Name: {organizerInfo.first_name}</p>
+          <p>Last Name: {organizerInfo.last_name}</p>
+          <p>Email: {organizerInfo.email}</p>
+          <p>Age: {organizerInfo.age}</p>
+        </div>
+      ) : (
+        <p>Loading organizer information...</p>
+      )}
+    </div>
+  );
+};
+
+export default OrganizerProfile;
