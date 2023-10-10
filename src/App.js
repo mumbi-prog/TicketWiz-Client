256e9780c4187e1aef631957ef4915fbf4bcfd69
Ken <kabutukenneth3@gmail.com>
2023-10-10 06:35:32 +0300

    add organizerroutes on app.js and wrap the app.js with the router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import Navbar from './components/LandingPage/Navbar';
 import HeroSection from './components/LandingPage/heroSection';
@@ -5,17 +6,23 @@ import FeaturedEvents from './components/LandingPage/FeaturedEvents';
 import Clients from './components/LandingPage/Clients';
 import Newevents from './components/LandingPage/Newevents';
 import Footer from './components/LandingPage/Footer';
+import { BrowserRouter as Router } from 'react-router-dom';
+import OrganizerRoutes from './OrganizerRoutes';
 
 function App() {
   return (
-   <div>
-      <Navbar/>
-      <HeroSection/>
-      <FeaturedEvents/>
-      <Newevents />
-      <Clients />
-      <Footer />
-   </div>
+    <Router>   {/* Wrap the entire application with Router */}
+      <div>
+        <Navbar/>
+        <HeroSection/>
+        <FeaturedEvents/>
+        <Newevents />
+        <Clients />
+        <Footer />
+        <OrganizerRoutes /> {/* Include OrganizerRoutes component */}
+      </div>
+    </Router>
+  
   );
   }
 export default App;
