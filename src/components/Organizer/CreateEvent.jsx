811a7cfdadbb9fe68b0d4c35d11523256bc95945
Ken <kabutukenneth3@gmail.com>
2023-10-10 08:44:48 +0300

    handle submit and error handling

--- src/components/Organizer/CreateEvent.jsx
+++ src/components/Organizer/CreateEvent.jsx
@@ -22,5 +22,30 @@ const CreateEvent = () => {
         });
     };
 
-    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch('/api/events',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(eventDetails),
+            });
+            if (!response.ok) {
+                // event created successfully
+                console.log('Event created successfully!');
+                // redirect to event details page
+            } else {
+                //handle error cases
+                console.log('Failed to create event.')
+            }
+        }   catch (error) {
+            console.error('Error creating event:', error);
+        }
+    };
+
+
+    re
 }
