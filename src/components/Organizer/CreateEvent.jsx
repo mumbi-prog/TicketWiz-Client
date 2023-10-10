42ef50593aa96244d8d39da9af96b10311479a2f
Ken <kabutukenneth3@gmail.com>
2023-10-10 08:28:44 +0300

    create event details

--- src/components/Organizer/CreateEvent.jsx
+++ src/components/Organizer/CreateEvent.jsx
@@ -2,6 +2,15 @@ import React, { useState } from 'react';
 
 const CreateEvent = () => {
     const [eventDetails, setEventDeatails] = useState({
-        
+        title: '',
+        description: '',
+        category: '', // Add other event details fields here
+        image_url: '',
+        date: '',
+        start_time: '',
+        end_time: '',
+        venue_name: '',
+        event_location: '',
+        available_tickets_count: 0,
     })
 }
