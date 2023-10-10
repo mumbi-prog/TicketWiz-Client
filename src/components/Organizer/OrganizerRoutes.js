6b3764fa240088bb90a133ecdb7ba047d29cb46c
Ken <kabutukenneth3@gmail.com>
2023-10-10 06:23:07 +0300

    import dependancies on organizerroutes component

---
+++ src/components/Organizer/OrganizerRoutes.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import CreateEvent from './organizer/CreateEvent';
+import TicketSales from './organizer/TicketSales';
+import OrganizerProfile from './organizer/OrganizerProfile'
