065510d826241038ab3b5728d6969522dd5624fc
Ken <kabutukenneth3@gmail.com>
2023-10-10 06:26:56 +0300

    define routes in organizer folder

--- src/components/Organizer/OrganizerRoutes.js
+++ src/components/Organizer/OrganizerRoutes.js
@@ -3,3 +3,16 @@ import { Switch, Route } from 'react-router-dom';
 import CreateEvent from './organizer/CreateEvent';
 import TicketSales from './organizer/TicketSales';
 import OrganizerProfile from './organizer/OrganizerProfile'
+
+
+const OrganizerRoutes = () => {
+    return (
+        <Switch>
+            <Route path="/organizer/create-event" component={CreateEvent} />
+            <Route path="/organizer/ticket-sales" component={TicketSales} />
+            <Route path="/organizer/profile" component={OrganizerProfile} />
+        </Switch>
+    );
+};
+
+export default OrganizerRoutes;
