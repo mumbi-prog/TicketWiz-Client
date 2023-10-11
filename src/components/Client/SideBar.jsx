2612b627c28813ec5e602f29b38b5283af89c8ec
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-11 05:10:12 +0300

    Added necessary imports

---
+++ src/components/Client/SideBar.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import {FaUserAlt, FaBars, FaHistory} from 'react-icons/fa';
+import {RiDashboardFill} from 'react-icons/ri'
+import {BsCalendar2EventFill, BsFillTicketPerforatedFill} from 'react-icons/bs'
+import { NavLink } from 'react-router-dom';
+import logo from './../../images/logo.png'
+
+function SideBar() {
+  return (
+    <div>SideBar</div>
+  )
+}
+
+export default SideBar
