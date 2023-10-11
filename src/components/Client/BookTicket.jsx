30c4aea21be6f367a84d4183d91976b311397eee
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-11 05:05:30 +0300

    Added basic starter code for the components

---
+++ src/components/Client/BookTicket.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BookTicket() {
+  return (
+    <div>
+        <h1>BookTicket</h1>
+    </div>
+  )
+}
+
+export default BookTicket
---
+++ src/components/Client/TicketWallet.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TicketWallet() {
+  return (
+    <div>
+        <h1>TicketWallet</h1>
+    </div>
+  )
+}
+
+export default TicketWallet
