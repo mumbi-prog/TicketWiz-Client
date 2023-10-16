7e6ec258cb29c854411eea77a2424fb39a74f877
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-16 16:02:20 +0300

    Added formatDate function

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,5 +1,20 @@
 import React from 'react'
 
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const month = date.toLocaleDateString(undefined, { month: 'short' });
+  const day = date.getDate();
+  const year = date.getFullYear(); 
+  return (
+    <div className="event-date font-sans font-bold flex flex-col items-center justify-center mt-[2px]">
+      <p className="month text-main-blue font-medium text-sm">{month}</p>
+      <p className="day text-3xl mt-[-2px]">{day}</p>
+      <p className="year text-sm text-gray-500 mt-1">{year}</p> 
+    </div>
+  );
+}
+
+
 function EventDetails() {
   return (
     <div>EventDetails</div>
