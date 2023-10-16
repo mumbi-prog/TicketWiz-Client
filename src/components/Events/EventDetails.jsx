f7a9cd4f021203c6ccc558075288dc98278a89b5
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-16 16:26:54 +0300

    Render event details

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,4 +1,6 @@
 import React, {useEffect, useState} from 'react'
+import { FaLocationDot, FaRegClock } from 'react-icons/fa6';
+import './Events.css';
 
 function formatDate(dateString) {
   const date = new Date(dateString);
@@ -26,7 +28,30 @@ function EventDetails({ eventId }) {
   }, [eventId]);
 
   return (
-    <div>EventDetails</div>
+    <div className="event-details">
+      <div className="event-image">
+        <img src={event.image_url} alt={event.title} />
+      </div>
+      <div className="event-info">
+        <div className="date-and-title">
+          <p>Date: {formatDate(event.date)}</p>
+          <h2 className="text-xl font-bold mb-3">{event.title}</h2>
+        </div>
+        <div className="venue1 flex items-center">
+          <p className="text-black text-md mr-[5px] mt-[5px]"><FaLocationDot className="location-icon1" /></p>
+          <p className='local'>Venue: {event.venue_name}, {event.event_location}</p>
+        </div>
+        <div className="venue1 flex items-center">
+          <p className="text-black text-md mr-[5px] mt-[5px]"><FaRegClock className="location-icon1" /></p>
+          <p>{event.start_time} to {event.end_time}</p>
+        </div>
+        <p className='event-desc'>{event.description}</p>
+      </div>
+      <div className="event-actions">
+        <button className="btn btn-primary">Get Ticket</button>
+        <button className="btn btn-secondary">Add to Calendar</button>
+      </div>
+    </div>
   )
 }
 
