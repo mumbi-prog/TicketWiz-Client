cfb7c90195637b5173a4a608dbf6c223c1559ecc
Ken <kabutukenneth3@gmail.com>
2023-10-16 21:17:54 +0300

    create organizerEvents

--- src/components/Organizer/Dashboard.jsx
+++ src/components/Organizer/Dashboard.jsx
@@ -4,6 +4,9 @@ const Dashboard = () => {
   const [totalEvents, setTotalEvents] = useState(0);
   const [totalTicketsSold, setTotalTicketsSold] = useState(0);
   const [totalRevenue, setTotalRevenue] = useState(0);
+  const [organizerEvents, setOrganizerEvents] = useState([]);
+
+  // 
 }
 
 export default Dashboard
