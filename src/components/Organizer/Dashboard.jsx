27a7b89b3381aedf1412edaa0d6df6139fb35e8d
Ken <kabutukenneth3@gmail.com>
2023-10-16 21:27:59 +0300

    create organizer's create events card

--- src/components/Organizer/Dashboard.jsx
+++ src/components/Organizer/Dashboard.jsx
@@ -16,19 +16,33 @@ const Dashboard = () => {
 
   return (
     <div>
-    <h2>Organizer Dashboard</h2>
-    <div>
+      <h2>Organizer Dashboard</h2>
       <div>
-        <p>Total Events: {totalEvents}</p>
-      </div>
-      <div>
-        <p>Total Tickets Sold: {totalTicketsSold}</p>
-      </div>
-      <div>
-        <p>Total Revenue: ${totalRevenue}</p>
+        <div>
+          <p>Total Events: {totalEvents}</p>
+        </div>
+        <div>
+          <p>Total Tickets Sold: {totalTicketsSold}</p>
+        </div>
+        <div>
+          <p>Total Revenue: ${totalRevenue}</p>
+        </div>
+    </div>
+
+    <h3>Organizer's Created Events:</h3>
+      <div className="event-cards">
+        {organizerEvents.map((event) => (
+          <div className="event-card" key={event.id}>
+            <h4>{event.title}</h4>
+            <p>Date: {event.date}</p>
+            {/* Buttons for update and delete actions */}
+            <button>Edit</button>
+            <button>Delete</button>
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Dashboard
