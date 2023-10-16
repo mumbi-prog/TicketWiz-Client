e26b86d4b1d8657d2f106f85263ff624fd549864
Ken <kabutukenneth3@gmail.com>
2023-10-16 21:15:48 +0300

    create totalRevenue

--- src/components/Organizer/Dashboard.jsx
+++ src/components/Organizer/Dashboard.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 const Dashboard = () => {
   const [totalEvents, setTotalEvents] = useState(0);
   const [totalTicketsSold, setTotalTicketsSold] = useState(0);
-  
+  const [totalRevenue, setTotalRevenue] = useState(0);
 }
 
 export default Dashboard
