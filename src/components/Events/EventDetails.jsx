b019cf5f6a5ce119a46acf75f903b231a36fce04
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-17 06:10:01 +0300

    Finished styling and added format date function

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -8,12 +8,21 @@ function formatDate(dateString) {
   const day = date.getDate();
   const year = date.getFullYear(); 
   return (
-    <div className="event-date font-sans font-bold flex flex-col items-center justify-left mt-[2px] border border-black px-[15px] py-[5px] mr-[15px]">
-      <p className="month text-main-blue font-medium text-sm">{month}</p>
-      <p className="day text-3xl mt-[-2px] font-medium">{day}</p>
-      <p className="year text-main-blue font-medium text-sm">{year}</p> 
+    <div className="event-date bg-lighter-blue text-text-color font-sans font-bold flex flex-col items-center justify-left mt-[2px] border rounded-md border-black px-[18px] py-[8px] mr-[15px]">
+      <p className="month text-text-color text-main-blue font-medium text-sm">{month}</p>
+      <p className="day text-text-color text-3xl mt-[-2px] font-medium">{day}</p>
+      <p className="year text-text-color text-main-blue font-medium text-sm">{year}</p> 
     </div>
   );
+}
+
+function formatTime(timeString) {
+  const time = new Date(timeString);
+  const hours = time.getHours() % 12 || 12;
+  const minutes = time.getMinutes();
+  const ampm = time.getHours() >= 12 ? 'PM' : 'AM';
+
+  return `${hours}:${minutes < 10 ? '0' : ''}${minutes} ${ampm}`;
 }
 
 
@@ -32,31 +41,33 @@ function EventDetails({ eventId }) {
   }
 
   return (
-    <div className="event-details-container">
-      <div className="event-details">
+    <div className="event-details-container my-[40px] mx-[20px] tracking-wider tracking-wider">
+      <div className="event-details flex flex-row gap-[-10px] font-sans">
         <div className="event-image">
-          <img src={event.image_url} alt={event.title}/>
+          <img src={event.image_url} alt={event.title} className='w-[470px] h-[470px] ml-[-100px]'/>
         </div>
-        <div className="event-info">
-          <div className="date-and-title mb-[15px]">
+        <div className="event-info ">
+          <div className="date-and-title mb-[15px] flex">
             <p>{formatDate(event.date)}</p>
-            <h2 className="e-title text-xl font-bold mb-3 uppercase">{event.title}</h2>
+            <h2 className="e-title text-[35px] font-bold mb-3 uppercase">{event.title}</h2>
           </div>
-          <div className="venue1 flex items-center">
+          <div className="venue1 flex items-center mt-[30px]">
             <p className="text-black text-md mr-[10px] mt-[5px]"><FaLocationDot className="location-icon1" /></p>
-            <p className='local'>{event.venue_name}, {event.event_location}</p>
+            <p className='local uppercase font-medium '>{event.venue_name}, {event.event_location}</p>
           </div>
-          <div className="venue1 flex items-center">
+          <div className="venue1 flex items-center mb-[60px]">
             <p className="text-black text-md mr-[10px] mt-[5px]"><FaRegClock className="location-icon1" /></p>
-            <p>{event.start_time} to {event.end_time}</p>
+            <p className='font-medium'>{formatTime(event.start_time)} to {formatTime(event.end_time)}</p>
           </div>
           <p className='event-desc'>{event.description}</p>
+          <div className="event-actions  flex justify-between items-end gap-[60px] mt-[50px]">
+            <button className="btn btn-primary rounded-md text-sm italic bg-lighter-blue text-text-color font-sans font-bold uppercase px-[30px] py-[10px]">Get Ticket</button>
+            <button className="btn btn-secondary rounded-md text-sm italic bg-lighter-blue text-text-color font-sans font-bold uppercase  px-[30px] py-[10px]">Add to Calendar</button>
+          </div>
+
         </div>
       </div>
-      <div className="event-actions">
-        <button className="btn btn-primary">Get Ticket</button>
-        <button className="btn btn-secondary">Add to Calendar</button>
-      </div>
+     
     </div>
   )
 }
