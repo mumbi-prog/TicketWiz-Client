ad7e76dea0c9e2b395d866515887514b07fd7a48
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-19 19:17:14 +0300

    Added AddToCalendar functionality

---
+++ src/components/Events/AddToCalendar.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+function AddToCalendar({ event }) {
+  const handleAddToCalendar = () => {
+    const gapi = window.gapi;
+    const CLIENT_ID = "789640415717-nj366h6ak6ii831tgaukfbjc6aca5mig.apps.googleusercontent.com";
+    const API_KEY = "AIzaSyBlQGsfotOEILsvKGMj8nbL0_0NVpcZJAg";
+    const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
+    const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
+
+    gapi.load('client:auth2', () => {
+      gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: [DISCOVERY_DOC],
+        scope: SCOPES,
+      });
+
+      gapi.client.load('calendar', 'v3', () => {
+        gapi.auth2.getAuthInstance().signIn().then(() => {
+          const eventDetails = {
+            summary: event.title,
+            location: `${event.venue_name}, ${event.event_location}`,
+            description: event.description,
+            start: {
+              dateTime: event.start_time,
+              timeZone: 'Africa/Nairobi',
+            },
+            end: {
+              dateTime: event.end_time,
+              timeZone: 'Africa/Nairobi',
+            },
+          };
+
+          const request = gapi.client.calendar.events.insert({
+            calendarId: 'primary',
+            resource: eventDetails,
+          });
+
+          request.execute((event) => {
+            window.open(event.htmlLink);
+          });
+        });
+      });
+    });
+  };
+
+  return (
+    <button onClick={handleAddToCalendar} className="btn btn-secondary rounded-md text-sm italic bg-lighter-blue text-text-color font-sans font-bold uppercase px-[30px] py-[10px]">Add to Calendar</button>
+  );
+}
+
+export default AddToCalendar;
+
