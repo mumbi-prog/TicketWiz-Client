4ccecf86d4561f28ddc761ca453b5538329b0d57
Ken <kabutukenneth3@gmail.com>
2023-10-20 19:49:35 +0300

    Fetch data from api using useEffect

--- src/components/Client/CustomerEventHistory.jsx
+++ src/components/Client/CustomerEventHistory.jsx
@@ -2,4 +2,12 @@ import React, { useState, useEffect } from 'react';
 
 const CustomerEventHistory = () => {
     const [eventHistory, setEventHistory] = useState([]);
+
+    // Fetch data from API
+    useEffect(() => {
+        fetch('/api/customer-events')
+        .then(response => response.json())
+        .then(data => setEventHistory(data))
+        .catch((error) => console.error('Error fetching event history', error));
+    }, []);
 }
