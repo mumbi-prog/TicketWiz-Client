26e45f6b13194fc510b4b0f5e3bab42a630f0934
Njerimacharia98 <njerimacharia05@gmail.com>
2023-10-23 01:01:49 +0300

    create account redirects to log in

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -1,39 +1,151 @@
-import React from "react";
-import logo from './../../images/logo.png';
-import { Link } from "react-router-dom";
+// import React from "react";
+// import logo from './../../images/logo.png';
+// import { Link } from "react-router-dom";
+// import PasswordInput from "../passwordInput";
+
+
+// const LoginPage = () => {
+//   return (
+//     <div className="flex h-screen">
+      
+//       <div className="flex justify-center items-center w-[100%] bg-gradient-to-r from-gradient-color1 to-gradient-color2 p-[5px] pr-[30px ">
+//         <div className="w-80 h-[80%] mr-20 p-8 rounded shadow">
+//           <h1 className="text-3xl font-bold mb-14 mt-[75%] text-center text-white">
+//           <Link to="/"> 
+//               TicketWiz
+//             </Link>
+//           </h1>
+//           <img
+//            src={logo} alt="Logo" 
+//             className="w-48 h-48 rounded-full ml-[15%]"
+//           />
+//         </div>
+//         <div className="w-96 p-8 rounded shadow ml-20 mt-10">
+//           <h2 className="text-4xl font-bold mb-4 text-center text-black">
+//             Login
+//           </h2>{" "}
+//           <h2 className="text-2xl font-bold mb-4 text-center text-black">
+//             Welcome Back
+//           </h2>{" "}
+//           <h2 className="text-1xl font-bold mb-4 text-center text-black">
+//             Don't have an account?{" "}
+//             <Link to="/signup">
+//               <span className=" font-bold mb-4 text-black underline">Signup</span>
+//             </Link>{" "}
+//             instead.
+//           </h2>{" "}
+          
+//           <form>
+//             <div className="mb-4">
+//               <label
+//                 htmlFor="email"
+//                 className="block text-sm font-medium text-gray-700"
+//               >
+//                 EMAIL
+//               </label>
+//               <input
+//                 placeholder="Email address"
+//                 type="email"
+//                 id="email"
+//                 name="email"
+//                 className="mt-1 p-2 w-full border rounded-md bg-white"
+//                 required
+//               />
+//             </div>
+//             <div className="mb-4">
+//             <PasswordInput name="password" title="PASSWORD" placeholder="Password"/>
+//             </div>
+//             <div className="text-center">
+//               <button
+//                 type="submit"
+//                 className="bg-blue-700 text-white font-semibold px-4 py-2 rounded-md hover-bg-blue-800 w-full mt-7"
+//               >
+//                 Log In
+//               </button>
+//             </div>
+//           </form>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default LoginPage;
+
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import PasswordInput from "../passwordInput";
+import logo from './../../images/logo.png';
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMsg, setErrMsg] = useState("");
+
+  const handleLogin = async () => {
+    console.log(email, password, "here are the information");
+    try {
+      const response = await fetch("your_login_api_endpoint_here", {
+        method: "POST",
+        body: JSON.stringify({ email, password }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        
+        const data = await response.json();
+        const receivedUsername = data.username;
+        localStorage.setItem("username", receivedUsername);
+
+        
+        navigate("/dashboard");
+      } else {
+       
+        setErrMsg("An error occurred while logging in");
+        setTimeout(() => {
+          setErrMsg("");
+        }, 3000); 
+      }
+    } catch (error) {
+     
+      setErrMsg("An error occurred while logging in");
+      setTimeout(() => {
+        setErrMsg("");
+      }, 3000); 
+    }
+  };
+
   return (
     <div className="flex h-screen">
-      
-      <div className="flex justify-center items-center w-[100%] bg-gradient-to-r from-gradient-color1 to-gradient-color2 p-[5px] pr-[30px ">
+      <div className="flex justify-center items-center w-[100%] bg-gradient-to-r from-gradient-color1 to-gradient-color2 p-[5px] pr-[30px]">
         <div className="w-80 h-[80%] mr-20 p-8 rounded shadow">
           <h1 className="text-3xl font-bold mb-14 mt-[75%] text-center text-white">
-          <Link to="/"> 
-              TicketWiz
-            </Link>
+            <Link to="/">TicketWiz</Link>
           </h1>
           <img
-           src={logo} alt="Logo" 
+            src={logo}
+            alt="Logo"
             className="w-48 h-48 rounded-full ml-[15%]"
           />
         </div>
         <div className="w-96 p-8 rounded shadow ml-20 mt-10">
           <h2 className="text-4xl font-bold mb-4 text-center text-black">
             Login
-          </h2>{" "}
+          </h2>
           <h2 className="text-2xl font-bold mb-4 text-center text-black">
             Welcome Back
-          </h2>{" "}
+          </h2>
           <h2 className="text-1xl font-bold mb-4 text-center text-black">
             Don't have an account?{" "}
             <Link to="/signup">
-              <span className=" font-bold mb-4 text-black underline">Signup</span>
+              <span className="font-bold mb-4 text-black underline">Signup</span>
             </Link>{" "}
             instead.
-          </h2>{" "}
-          
+          </h2>
+
           <form>
             <div className="mb-4">
               <label
@@ -49,20 +161,35 @@ const LoginPage = () => {
                 name="email"
                 className="mt-1 p-2 w-full border rounded-md bg-white"
                 required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div className="mb-4">
-            <PasswordInput name="password" title="PASSWORD" placeholder="Password"/>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                PASSWORD
+              </label>
+              <PasswordInput
+                name="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
             <div className="text-center">
               <button
                 type="submit"
                 className="bg-blue-700 text-white font-semibold px-4 py-2 rounded-md hover-bg-blue-800 w-full mt-7"
+                onClick={handleLogin}
               >
                 Log In
               </button>
             </div>
           </form>
+          <p>{errorMsg}</p>
         </div>
       </div>
     </div>
@@ -70,3 +197,4 @@ const LoginPage = () => {
 };
 
 export default LoginPage;
+
--- src/components/Login/signup.jsx
+++ src/components/Login/signup.jsx
@@ -1,115 +1,283 @@
-import React from "react";
+// import React from "react";
+// import logo from './../../images/logo.png';
+// import { Link } from "react-router-dom";
+// import PasswordInput from "../passwordInput";
+
+// const SignUp = () => {
+
+  
+//   const handleCreateAccount = (e) => {
+//     e.preventDefault();
+//     window.alert("Your account has been created!");
+//   };
+
+  
+//   return (
+//     <div className="flex h-screen">
+//       <div className="flex justify-center items-center w-[100%] bg-gradient-to-r from-gradient-color1 to-gradient-color2 p-[5px] pr-[30px]">
+//         <div className="w-80 h-[80%] mr-20 p-8 rounded shadow">
+//           <h1 className="text-3xl font-bold mb-14 mt-[50%] text-center text-white">
+//             <Link to="/">TicketWiz</Link>
+//           </h1>
+//           <img
+//             src={logo} alt="Logo"
+//             className="w-48 h-48 rounded-full ml-[15%]"
+//           />
+//         </div>
+//         <div className="w-96 p-8 rounded shadow ml-20 mt-10">
+//           <h2 className="text-5xl font-bold mb-4 text-center text-black">
+//             Register
+//           </h2>
+//           <h2 className="text-3xl font-bold mb-2 text-center text-black">
+//             It's completely free.
+//           </h2>
+
+//           <form>
+//             <div className="mb-2">
+//               <label
+//                 htmlFor="text"
+//                 className="block text-sm font-medium text-gray-700"
+//               >
+//                 NAME
+//               </label>
+//               <input
+//                 placeholder="Your full name"
+//                 type="text"
+//                 id="name"
+//                 name="name"
+//                 className="mt-1 p-2 w-full border rounded-md bg-white"
+//                 required
+//               />
+//             </div>
+//             <div className="mb-2">
+//               <label
+//                 htmlFor="text"
+//                 className="block text-sm font-medium text-gray-700"
+//               >
+//                 USERNAME
+//               </label>
+//               <input
+//                 placeholder="Username"
+//                 type="text"
+//                 id="username"
+//                 name="username"
+//                 className="mt-1 p-2 w-full border rounded-md bg-white"
+//                 required
+//               />
+//             </div>
+//             <div className="mb-2">
+//               <label
+//                 htmlFor="email"
+//                 className="block text-sm font-medium text-gray-700"
+//               >
+//                 EMAIL
+//               </label>
+//               <input
+//                 placeholder="Email address"
+//                 type="email"
+//                 id="email"
+//                 name="email"
+//                 className="mt-1 p-2 w-full border rounded-md bg-white"
+//                 required
+//               />
+//             </div>
+//             <div className="mb-2">
+  
+//               <div className="relative">
+//               <PasswordInput name="password" title="PASSWORD" placeholder="Password"/>
+//               </div>
+//             </div>
+//             <div className="mb-2">
+//             <PasswordInput name="password" title="CONFIRM PASSWORD" placeholder="Confirm Password" />
+//             </div>
+//             <div className="text-center">
+//               <button
+//                 type="submit"
+//                 className="bg-blue-700 text-white font-semibold px-4 py-2 rounded-md hover:bg-blue-800 w-full mt-7"
+//                 onClick={handleCreateAccount}
+//               >
+//                 CREATE ACCOUNT
+//               </button>
+//             </div>
+//           </form>
+//           <h2 className="text-1xl font-bold mb-4 text-center text-black">
+//             Already have an account?{" "}
+//             <Link to="/login">
+//               <span className="font-bold mb-4 text-black underline">Login</span>
+//             </Link>{" "}
+//             instead.
+//           </h2>{" "}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default SignUp;
+
+import React, { useState } from "react";
 import logo from './../../images/logo.png';
-import { Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom"; 
 import PasswordInput from "../passwordInput";
 
 const SignUp = () => {
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errMsg, setErrMsg] = useState("");
 
-  
-  const handleCreateAccount = (e) => {
-    e.preventDefault();
-    window.alert("Your account has been created!");
+  const handleAddAccount = async () => {
+    try {
+      const isPasswordValid = password === confirmPassword;
+      if (!isPasswordValid) {
+        setErrMsg("Password Mismatch!");
+      } else {
+        const response = await fetch("your_login_api_endpoint_here", {
+          method: "POST",
+          body: JSON.stringify({
+            email,
+            password,
+            username,
+            name,
+            confirmPassword,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        if (response.ok) {
+          
+          navigate("/login");
+        } else {
+          
+          setErrMsg("An error occurred while creating an account");
+          setTimeout(() => {
+            setErrMsg("");
+          }, 3000);
+        }
+      }
+    } catch (err) {
+      setErrMsg("An error occurred");
+      setTimeout(() => {
+        setErrMsg("");
+      }, 3000); 
+    }
   };
 
-  
   return (
-    <div className="flex h-screen">
-      <div className="flex justify-center items-center w-[100%] bg-gradient-to-r from-gradient-color1 to-gradient-color2 p-[5px] pr-[30px]">
-        <div className="w-80 h-[80%] mr-20 p-8 rounded shadow">
-          <h1 className="text-3xl font-bold mb-14 mt-[50%] text-center text-white">
-            <Link to="/">TicketWiz</Link>
-          </h1>
-          <img
-            src={logo} alt="Logo"
-            className="w-48 h-48 rounded-full ml-[15%]"
-          />
-        </div>
-        <div className="w-96 p-8 rounded shadow ml-20 mt-10">
-          <h2 className="text-5xl font-bold mb-4 text-center text-black">
-            Register
-          </h2>
-          <h2 className="text-3xl font-bold mb-2 text-center text-black">
-            It's completely free.
-          </h2>
-
-          <form>
-            <div className="mb-2">
-              <label
-                htmlFor="text"
-                className="block text-sm font-medium text-gray-700"
-              >
-                NAME
-              </label>
-              <input
-                placeholder="Your full name"
-                type="text"
-                id="name"
-                name="name"
-                className="mt-1 p-2 w-full border rounded-md bg-white"
-                required
-              />
-            </div>
-            <div className="mb-2">
-              <label
-                htmlFor="text"
-                className="block text-sm font-medium text-gray-700"
-              >
-                USERNAME
-              </label>
-              <input
-                placeholder="Username"
-                type="text"
-                id="username"
-                name="username"
-                className="mt-1 p-2 w-full border rounded-md bg-white"
-                required
-              />
-            </div>
-            <div className="mb-2">
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                EMAIL
-              </label>
-              <input
-                placeholder="Email address"
-                type="email"
-                id="email"
-                name="email"
-                className="mt-1 p-2 w-full border rounded-md bg-white"
-                required
-              />
-            </div>
-            <div className="mb-2">
-  
-              <div className="relative">
-              <PasswordInput name="password" title="PASSWORD" placeholder="Password"/>
+    <>
+      <p>{errMsg}</p>
+      <div className="flex h-screen">
+        
+        <div className="flex justify-center items-center w-[100%] bg-gradient-to-r from-gradient-color1 to-gradient-color2 p-[5px] pr-[30px]">
+          <div className="w-80 h-[80%] mr-20 p-8 rounded shadow">
+            <h1 className="text-3xl font-bold mb-14 mt-[50%] text-center text-white">
+              <Link to="/">TicketWiz</Link>
+            </h1>
+            <img
+              src={logo} alt="Logo"
+              className="w-48 h-48 rounded-full ml-[15%]"
+            />
+          </div>
+          <div className="w-96 p-8 rounded shadow ml-20 mt-10">
+            <h2 className="text-5xl font-bold mb-4 text-center text-black">
+              Register
+            </h2>
+            <h2 className="text-3xl font-bold mb-2 text-center text-black">
+              It's completely free.
+            </h2>{" "}
+            <form>
+              <div className="mb-2">
+                <label
+                  htmlFor="text"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  NAME
+                </label>
+                <input
+                  placeholder="Your full name"
+                  type="text"
+                  id="name"
+                  name="name"
+                  className="mt-1 p-2 w-full border rounded-md bg-white"
+                  required
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
               </div>
-            </div>
-            <div className="mb-2">
-            <PasswordInput name="password" title="CONFIRM PASSWORD" placeholder="Confirm Password" />
-            </div>
-            <div className="text-center">
-              <button
-                type="submit"
-                className="bg-blue-700 text-white font-semibold px-4 py-2 rounded-md hover:bg-blue-800 w-full mt-7"
-                onClick={handleCreateAccount}
-              >
-                CREATE ACCOUNT
-              </button>
-            </div>
-          </form>
-          <h2 className="text-1xl font-bold mb-4 text-center text-black">
-            Already have an account?{" "}
-            <Link to="/login">
-              <span className="font-bold mb-4 text-black underline">Login</span>
-            </Link>{" "}
-            instead.
-          </h2>{" "}
+              <div className="mb-2">
+                <label
+                  htmlFor="text"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  USERNAME
+                </label>
+                <input
+                  placeholder="Username"
+                  type="text"
+                  id="username"
+                  name="username"
+                  className="mt-1 p-2 w-full border rounded-md bg-white"
+                  required
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+              <div className="mb-2">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  EMAIL
+                </label>
+                <input
+                  placeholder="Email address"
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="mt-1 p-2 w-full border rounded-md bg-white"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="mb-2">
+                <div className="relative">
+                  <PasswordInput
+                    name="password"
+                    title="PASSWORD"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+              </div>
+              <div className="mb-2">
+                <PasswordInput
+                  name="password"
+                  title="CONFIRM PASSWORD"
+                  placeholder="Confirm Password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+              </div>
+              <div className="text-center">
+                <button
+                  type="submit"
+                  className="bg-blue-700 text-white font-semibold px-4 py-2 rounded-md hover:bg-blue-800 w-full mt-7"
+                  onClick={handleAddAccount}
+                >
+                  CREATE ACCOUNT
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
