920064d3157853d214e24714dc1b05eb0d091c2f
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-23 17:14:33 +0300

    Render payment

--- src/components/Events/Payment.jsx
+++ src/components/Events/Payment.jsx
@@ -61,7 +61,38 @@ function Payment({ event }) {
   }; 
 
   return (
-    <div>Payment</div>
+    <div>
+      <h1>Payment Page</h1>
+      <h2>Event Details</h2>
+      <p>Title: {event.title}</p>
+      <p>Time: {event.start_time} to {event.end_time}</p>
+      <p>Price: Ksh. {event.price}</p>
+
+      <h2>Choose Your Ticket</h2>
+      <select value={ticketType} onChange={(e) => setTicketType(e.target.value)}>
+        <option value="MVP">MVP</option>
+        <option value="Early Booking">Early Booking</option>
+        <option value="Regular">Regular</option>
+      </select>
+
+      <h2>Choose Quantity</h2>
+      <input
+        type="number"
+        value={quantity}
+        min={1}
+        max={10}
+        onChange={(e) => setQuantity(e.target.value)}
+      />
+
+      <h2>Total Price</h2>
+      <p>Ksh. {calculatePrice()}</p>
+      <button
+        onClick={handlePayment}
+        className="btn btn-primary rounded-md text-sm italic bg-lighter-blue text-text-color font-sans font-bold uppercase px-[30px] py-[10px]"
+      >
+        Confirm Payment
+      </button>
+    </div>
   )
 }
 
