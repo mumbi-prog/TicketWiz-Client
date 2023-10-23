c7700b546430d4465c5f228b23adfe44d6425779
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-23 16:59:21 +0300

    Added function to calculate the price based on the selected ticket type and quantity

--- src/components/Events/Payment.jsx
+++ src/components/Events/Payment.jsx
@@ -3,6 +3,20 @@ import React, {useState} from 'react'
 function Payment({ event }) {
   const [ticketType, setTicketType] = useState('MVP');
   const [quantity, setQuantity] = useState(1);
+
+  const calculatePrice = () => {
+    const basePrice = event.price;
+    let price = basePrice;
+
+    if (ticketType === 'MVP') {
+      price = basePrice * 1.3;
+    } else if (ticketType === 'Early Booking') {
+      price = basePrice * 0.8;
+    }
+
+    return price * quantity;
+  };
+  
   return (
     <div>Payment</div>
   )
