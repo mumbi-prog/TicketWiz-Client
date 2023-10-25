2abc81cf945769e3c4483a58b171eeead9b2c90d
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-25 18:20:20 +0300

    Display customer info

--- src/components/Client/Profile.jsx
+++ src/components/Client/Profile.jsx
@@ -21,7 +21,26 @@ function Profile() {
   return (
     <div>
       <h1>Customer Profile</h1>
+      <form>
+        <div className="form-group">
+          <label htmlFor="first_name">First Name:</label>
+          <p id="first_name">{customerData.first_name}</p>
+        </div>
+        <div className="form-group">
+          <label htmlFor="last_name">Last Name:</label>
+          <p id="last_name">{customerData.last_name}</p>
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <p id="email">{customerData.email}</p>
+        </div>
+        <div className="form-group">
+          <label htmlFor="age">Age:</label>
+          <p id="age">{customerData.age}</p>
+        </div>
+      </form>
     </div>
+
   );
 }
 
