acd2c4818151150017a4cd3cf857fd9c8ced7401
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-25 16:23:57 +0300

    updates

--- src/components/Events/AddToCalendar.jsx
+++ src/components/Events/AddToCalendar.jsx
@@ -1,38 +1,38 @@
 import React from 'react';
 
 function AddToCalendar({ event }) {
-  const handleAddToCalendar = () => {
-    const gapi = window.gapi;
-    const CLIENT_ID = "789640415717-nj366h6ak6ii831tgaukfbjc6aca5mig.apps.googleusercontent.com";
-    const API_KEY = "AIzaSyBlQGsfotOEILsvKGMj8nbL0_0NVpcZJAg";
-    const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
-    const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
+  var gapi = window.gapi;
+  var CLIENT_ID = "985329526084-njirq449h3as451ek3a0apocmif6n3s8.apps.googleusercontent.com";
+  var API_KEY = "AIzaSyDGVy3u5_Ah5dQpiwEH87Ix9NvnRkAOEIw";
+  var DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
+  var SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
 
+  const handleAddToCalendar = () => {
     gapi.load('client:auth2', () => {
       gapi.client.init({
         apiKey: API_KEY,
         clientId: CLIENT_ID,
-        discoveryDocs: [DISCOVERY_DOC],
+        discoveryDocs: DISCOVERY_DOC,
         scope: SCOPES,
       });
 
       gapi.client.load('calendar', 'v3', () => {
         gapi.auth2.getAuthInstance().signIn().then(() => {
-          const eventDetails = {
-            summary: event.title,
-            location: `${event.venue_name}, ${event.event_location}`,
-            description: event.description,
-            start: {
-              dateTime: event.start_time,
-              timeZone: 'Africa/Nairobi',
+          var eventDetails = {
+            'summary': event.title,
+            'location': `${event.venue_name}, ${event.event_location}`,
+            'description': event.description,
+            'start': {
+              'dateTime': event.start_time,
+              'timeZone': 'Africa/Nairobi',
             },
-            end: {
-              dateTime: event.end_time,
-              timeZone: 'Africa/Nairobi',
+            'end': {
+              'dateTime': event.end_time,
+              'timeZone': 'Africa/Nairobi',
             },
           };
 
-          const request = gapi.client.calendar.events.insert({
+          var request = gapi.client.calendar.events.insert({
             calendarId: 'primary',
             resource: eventDetails,
           });
@@ -46,7 +46,12 @@ function AddToCalendar({ event }) {
   };
 
   return (
-    <button onClick={handleAddToCalendar} className="btn btn-secondary rounded-md text-sm italic bg-lighter-blue text-text-color font-sans font-bold uppercase px-[30px] py-[10px]">Add to Calendar</button>
+    <button
+      onClick={handleAddToCalendar}
+      className="btn btn-secondary rounded-md text-sm italic bg-lighter-blue text-text-color font-sans font-bold uppercase px-[30px] py-[10px]"
+    >
+      Add to Calendar
+    </button>
   );
 }
 
