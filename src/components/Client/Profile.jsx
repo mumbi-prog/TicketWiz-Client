6e8d2a9dae87b5dc8241557db0a732245fc65e60
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-25 18:15:09 +0300

    fetch the customer profile

--- src/components/Client/Profile.jsx
+++ src/components/Client/Profile.jsx
@@ -4,6 +4,20 @@ function Profile() {
   const [customerData, setCustomerData] = useState({});
   const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    fetch('/customer_profile')
+      .then((response) => response.json())
+      .then((data) => {
+        setCustomerData(data);
+        setLoading(false);
+      })
+      .catch((error) => console.error('Error fetching customer profile:', error));
+  }, []);
+
+  if (loading) {
+    return <div>Loading customer profile...</div>;
+  }
+
   return (
     <div>
       <h1>Customer Profile</h1>
