44d3a74cdbb5020ca9013b81e7ebdb3e7bbb6cef
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-25 18:14:27 +0300

    Initial state setup for customerData and loading states

--- src/components/Client/Profile.jsx
+++ src/components/Client/Profile.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 function Profile() {
+  const [customerData, setCustomerData] = useState({});
+  const [loading, setLoading] = useState(true);
+
   return (
     <div>
-        <h1>Customer Profile</h1>
+      <h1>Customer Profile</h1>
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
