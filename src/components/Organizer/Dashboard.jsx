f7e60106b7cf8e8bfc5847afc1b872b9ca4eaaea
Njerimacharia98 <njerimacharia05@gmail.com>
2023-10-25 17:45:32 +0300

    dashboard styling

--- src/components/Organizer/Dashboard.jsx
+++ src/components/Organizer/Dashboard.jsx
@@ -97,7 +97,7 @@
 //         </div>
 //     </div>
 
-//     <h3>Organizer's Created Events:</h3>
+//     <h3>My Events:</h3>
 //       <div className="event-cards">
 //         {organizerEvents.map((event) => (
 //           <div className="event-card" key={event.id}>
@@ -114,19 +114,167 @@
 //   );
 // };
 
-// export default Dashboard
+// export default Dashboard;
+
+// import React, { useEffect, useState } from 'react';
+
+// const Dashboard = () => {
+//   const [totalEvents, setTotalEvents] = useState(0);
+//   const [totalTicketsSold, setTotalTicketsSold] = useState(0);
+//   const [totalRevenue, setTotalRevenue] = useState(0);
+//   const [organizerEvents, setOrganizerEvents] = useState([]);
+
+//   // Fetch data from API
+//   useEffect(() => {
+//     // Fetch organizer's events
+//     fetch('http://localhost:4000/api/events')
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setOrganizerEvents(data);
+//       })
+//       .catch((error) => {
+//         console.error('Error fetching organizer events', error);
+//       });
+
+//     // Fetch organizer metrics
+//     fetch('http://localhost:4000/api/metrics')
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setTotalEvents(data.totalEvents);
+//         setTotalTicketsSold(data.totalTicketsSold);
+//         setTotalRevenue(data.totalRevenue);
+//       })
+//       .catch((error) => {
+//         console.error('Error fetching organizer metrics', error);
+//       });
+//   }, []);
+
+//   const handleUpdateEvent = (eventId) => {
+//     const updatedEventDetails = {
+//       title: "New Title",
+//       description: "New Description",
+//       category: 'New category',
+//       image_url: 'New img poster',
+//       date: 'New date',
+//       start_time: 'New start time',
+//       end_time: 'New end time',
+//       venue_name: 'New venue',
+//       event_location: 'New event location',
+//       available_tickets_count: 0,
+//     };
+//     fetch(`http://localhost:8000/api/event/${eventId}`, {
+//       method: 'PUT',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(updatedEventDetails),
+//     })
+//       .then((response) => response.json())
+//       .then((data) => {
+//         console.log('Event updated successfully');
+//       })
+//       .catch((error) => {
+//         console.error('Error updating event', error);
+//       });
+//   };
+
+//   const handleDeleteEvent = (eventId) => {
+//     if (window.confirm('Are you sure you want to delete this event?')) {
+//       fetch(`http://localhost:8000/api/event/${eventId}`, {
+//         method: 'DELETE',
+//       })
+//         .then((response) => {
+//           if (response.status === 204) {
+            
+//             console.log('Event deleted successfully');
+//           } else {
+            
+//             console.error('Error deleting event');
+//           }
+//         })
+//         .catch((error) => {
+//           console.error('Error deleting event', error);
+//         });
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h2 className="text-3xl font-bold mb-4">Dashboard</h2>
+//       <div className="flex flex-wrap justify-left ml-10">
+//         <div className="max-w-xs mx-4 mb-8">
+//           <div className="bg-gray-300 hover:bg-blue-500 rounded-lg shadow-lg transition duration-300 ease-in-out">
+//             <div className="p-4">
+//               <h2 className="text-2xl font-semibold mb-2 hover:text-white">
+//                 Total Events
+//                 </h2>
+//               <p className="text-gray-600 hover:text-white">{totalEvents}</p>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="max-w-xs mx-4 mb-8">
+//           <div className="bg-gray-300 hover:bg-blue-500 rounded-lg shadow-lg transition duration-300 ease-in-out">
+//             <div className="p-4">
+//               <h2 className="text-2xl font-semibold mb-2 hover:text-white">
+//                 Total Tickets Sold
+//               </h2>
+//               <p className="text-gray-600 hover:text-white">{totalTicketsSold}</p>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="max-w-xs mx-4 mb-8">
+//           <div className="bg-gray-300 hover:bg-blue-500 rounded-lg shadow-lg transition duration-300 ease-in-out">
+//             <div className="p-4">
+//               <h2 className="text-2xl font-semibold mb-2 hover:text-white">
+//                 Total Revenue
+//               </h2>
+//               <p className="text-gray-600 hover-text-white">${totalRevenue}</p>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+
+//       <h3 className="text-3xl font-bold mb-4">Organizer's Created Events:</h3>
+//       <div className="event-cards">
+//         {organizerEvents.map((event) => (
+//           <div className="event-card" key={event.id}>
+//             <h4 className="text-2xl font-semibold mb-2">{event.title}</h4>
+//             <p className="text-gray-600">Date: {event.date}</p>
+//             <button className="bg-blue-700 text-white font-semibold px-4 py-2 rounded-md hover:bg-blue-800" onClick={() => handleUpdateEvent(event.id)}>Edit</button>
+//             <button className="bg-red-700 text-white font-semibold px-4 py-2 rounded-md hover:bg-red-800" onClick={() => handleDeleteEvent(event.id)}>Delete</button>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Dashboard;
 
 import React, { useEffect, useState } from 'react';
+import MyEvents from './MyEvents';
+
+const Greetings = ({ username }) => {
+  return (
+    <div className="ml-10 mb-10 mt-10">
+      <h2 className="text-3xl font-semibold text-navy-blue">
+        Welcome, {username}
+      </h2>
+      <p className="text-xl mt-2 font-semibold">Organizer Dashboard</p>
+      
+    </div>
+  );
+};
 
 const Dashboard = () => {
-  const [totalEvents, setTotalEvents] = useState(0);
-  const [totalTicketsSold, setTotalTicketsSold] = useState(0);
-  const [totalRevenue, setTotalRevenue] = useState(0);
+  const [totalEvents, setTotalEvents] = useState(14);
+  const [totalTicketsSold, setTotalTicketsSold] = useState(223);
+  const [totalRevenue, setTotalRevenue] = useState(61990);
   const [organizerEvents, setOrganizerEvents] = useState([]);
 
-  // Fetch data from API
+  
   useEffect(() => {
-    // Fetch organizer's events
+    
     fetch('http://localhost:4000/api/events')
       .then((response) => response.json())
       .then((data) => {
@@ -136,7 +284,7 @@ const Dashboard = () => {
         console.error('Error fetching organizer events', error);
       });
 
-    // Fetch organizer metrics
+    
     fetch('http://localhost:4000/api/metrics')
       .then((response) => response.json())
       .then((data) => {
@@ -151,8 +299,8 @@ const Dashboard = () => {
 
   const handleUpdateEvent = (eventId) => {
     const updatedEventDetails = {
-      title: "New Title",
-      description: "New Description",
+      title: 'New Title',
+      description: 'New Description',
       category: 'New category',
       image_url: 'New img poster',
       date: 'New date',
@@ -185,10 +333,8 @@ const Dashboard = () => {
       })
         .then((response) => {
           if (response.status === 204) {
-            // Display success message
             console.log('Event deleted successfully');
           } else {
-            // Handle errors
             console.error('Error deleting event');
           }
         })
@@ -200,7 +346,8 @@ const Dashboard = () => {
 
   return (
     <div>
-      <h2 className="text-3xl font-bold mb-4">Dashboard</h2>
+      <Greetings username="YourUsername" />
+      {/* <h2 className="text-3xl font-bold mb-4">Dashboard</h2> */}
       <div className="flex flex-wrap justify-left ml-10">
         <div className="max-w-xs mx-4 mb-8">
           <div className="bg-gray-300 hover:bg-blue-500 rounded-lg shadow-lg transition duration-300 ease-in-out">
@@ -213,30 +360,31 @@ const Dashboard = () => {
           </div>
         </div>
         <div className="max-w-xs mx-4 mb-8">
-          <div className="bg-gray-300 hover-bg-blue-500 rounded-lg shadow-lg transition duration-300 ease-in-out">
+          <div className="bg-gray-300 hover:bg-blue-500 rounded-lg shadow-lg transition duration-300 ease-in-out">
             <div className="p-4">
-              <h2 className="text-2xl font-semibold mb-2 hover-text-white">
+              <h2 className="text-2xl font-semibold mb-2 hover:text-white">
                 Total Tickets Sold
               </h2>
-              <p className="text-gray-600 hover-text-white">{totalTicketsSold}</p>
+              <p className="text-gray-600 hover:text-white">{totalTicketsSold}</p>
             </div>
           </div>
         </div>
         <div className="max-w-xs mx-4 mb-8">
-          <div className="bg-gray-300 hover-bg-blue-500 rounded-lg shadow-lg transition duration-300 ease-in-out">
+          <div className="bg-gray-300 hover:bg-blue-500 rounded-lg shadow-lg transition duration-300 ease-in-out">
             <div className="p-4">
-              <h2 className="text-2xl font-semibold mb-2 hover-text-white">
+              <h2 className="text-2xl font-semibold mb-2 hover:text-white">
                 Total Revenue
               </h2>
-              <p className="text-gray-600 hover-text-white">${totalRevenue}</p>
+              <p className="text-gray-600 hover:text-white">${totalRevenue}</p>
             </div>
           </div>
         </div>
       </div>
 
-      <h3 className="text-3xl font-bold mb-4">Organizer's Created Events:</h3>
       <div className="event-cards">
-        {organizerEvents.map((event) => (
+        <MyEvents /> 
+        {organizerEvents.map((event, index) => (
+          
           <div className="event-card" key={event.id}>
             <h4 className="text-2xl font-semibold mb-2">{event.title}</h4>
             <p className="text-gray-600">Date: {event.date}</p>
@@ -246,7 +394,10 @@ const Dashboard = () => {
         ))}
       </div>
     </div>
+    
   );
 };
 
 export default Dashboard;
+
+
