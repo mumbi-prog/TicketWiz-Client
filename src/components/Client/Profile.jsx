a188cd53b7f2f6825a5b2f774b543d7736f0abcb
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-25 21:37:37 +0300

    Styled the client profile

--- src/components/Client/Profile.jsx
+++ src/components/Client/Profile.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect }  from 'react';
+import "./LoaderStyling.css"
 
 function Profile() {
   const [customerData, setCustomerData] = useState({});
@@ -15,28 +16,31 @@ function Profile() {
   }, []);
 
   if (loading) {
-    return <div>Loading customer profile...</div>;
+    return (
+    
+    );
   }
 
   return (
-    <div>
-      <h1>Customer Profile</h1>
-      <form>
-        <div className="form-group">
-          <label htmlFor="first_name">First Name:</label>
-          <p id="first_name">{customerData.first_name}</p>
+    <div className='profile-container pl-[30%] mt-[80px]'>
+     <h1 className='profile-head mt-[50px] uppercase px-[50px] py-[7px] font-bold bg-acc-blue text-text-color text-lg w-[400px] text-center'>Contact information</h1>
+      <form className='profile-form flex flex-col items-left mt-[20px]'>
+        <div className="form-group align-left">
+          <label htmlFor="first_name" className='label font-dm-serif-text font-medium'>First Name:</label>
+          <p id="first_name" className='user-data rounded-md px-[20px] py-[5px] my-[5px] bg-payment-modal-color w-[400px]'> {customerData.first_name}</p>
+          {customerData.first_name}
         </div>
         <div className="form-group">
-          <label htmlFor="last_name">Last Name:</label>
-          <p id="last_name">{customerData.last_name}</p>
+          <label htmlFor="last_name" className='label font-dm-serif-text font-medium'>Last Name:</label>
+          <p id="last_name" className='user-data rounded-md px-[20px] py-[5px] my-[5px] bg-payment-modal-color w-[400px]'> {customerData.last_name}</p>
         </div>
         <div className="form-group">
-          <label htmlFor="email">Email:</label>
-          <p id="email">{customerData.email}</p>
+          <label htmlFor="email" className='label font-dm-serif-text font-medium'>Email:</label>
+          <p id="email" className='user-data rounded-md px-[20px] py-[5px] my-[5px] bg-payment-modal-color w-[400px]'> {customerData.email}</p>
         </div>
         <div className="form-group">
-          <label htmlFor="age">Age:</label>
-          <p id="age">{customerData.age}</p>
+          <label htmlFor="age" className='label font-dm-serif-text font-medium'>Phone Number:</label>
+          <p id="age" className='user-data rounded-md px-[20px] py-[5px] my-[5px] bg-payment-modal-color w-[400px]'> {customerData.phone_number}</p>
         </div>
       </form>
     </div>
