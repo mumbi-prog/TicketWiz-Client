9166b7803b50af819836c606694fffd69c1b3135
Njerimacharia98 <njerimacharia05@gmail.com>
2023-10-26 16:56:22 +0300

    .

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -4,7 +4,6 @@ import { Link, useNavigate } from "react-router-dom";
 import PasswordInput from "../passwordInput";
 import logo from './../../images/logo.png';
 
-
   const LoginPage = ({ setUserRole }) => {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
