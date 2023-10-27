1c53a7aa2df117b13362d5cbe0b8a57d9873f167
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-28 02:30:28 +0300

    Customer signup component update

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -34,7 +34,7 @@ const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/customerLogin" element={<CustomerLoginPage />} />
+      <Route path="/customerlogin" element={<CustomerLoginPage/>} />
       <Route path="/login" element={<LoginPage setUserRole={setUserRole} />} />
       <Route path="/signup" element={<SignUpPage />} />
       <Route path="/organizersignup" element={<OrganizerSignupPage />} />
@@ -49,35 +49,3 @@ const AllRoutes = () => {
 };
 
 export default AllRoutes;
-
-
-
-// import React, { useState } from "react";
-// import { Routes, Route } from "react-router-dom";
-
-// import LoginPage from "../components/Login/login";
-// import CustomerLoginPage from "../components/Login/customerLogin";
-// import OrganizerSignupPage from "../components/Login/organiserSignup";
-// import SignUpPage from "../components/Login/signup";
-// import OrganizerDashboard from "../components/Organizer/Dashboard";
-// import CustomerDashboard from "../components/Client/Dashboard";
-
-// const AllRoutes = () => {
-//   const [userRole, setUserRole] = useState(""); 
-//   return (
-//     <Routes>
-//       <Route path="/customerLogin" element={<CustomerLoginPage />} />
-//       <Route path="/login" element={<LoginPage setUserRole={setUserRole} />} />
-//       <Route path="/signup" element={<SignUpPage />} />
-//       <Route path="/organizersignup" element={<OrganizerSignupPage />} />
-//       {userRole === "organizier" && (
-//         <Route path="/dashboard" element={<OrganizerDashboard />} />
-//       )}
-//       {userRole === "customer" && (
-//         <Route path="/dashboard" element={<CustomerDashboard />} />
-//       )}
-//     </Routes>
-//   );
-// };
-
-// export default AllRoutes;
