11c61f565b3e68e9433d0e2f83f93852d6a7de12
Njerimacharia98 <njerimacharia05@gmail.com>
2023-10-29 15:12:20 +0300

    conflicts resolved

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -52,32 +52,3 @@ export default AllRoutes;
 
 
 
-// import React, { useState } from "react";
-// import { Routes, Route } from "react-router-dom";
-
-// import LoginPage from "../components/Login/login";
-// import CustomerLoginPage from "../components/Login/customerLogin";
-// import OrganizerSignupPage from "../components/Login/organiserSignup";
-// import SignUpPage from "../components/Login/signup";
-// import OrganizerDashboard from "../components/Organizer/Dashboard";
-// import CustomerDashboard from "../components/Client/Dashboard";
-
-// const AllRoutes = () => {
-//   const [userRole, setUserRole] = useState(""); 
-//   return (
-//     <Routes>
-//       <Route path="/customerLogin" element={<CustomerLoginPage />} />
-//       <Route path="/login" element={<LoginPage setUserRole={setUserRole} />} />
-//       <Route path="/signup" element={<SignUpPage />} />
-//       <Route path="/organizersignup" element={<OrganizerSignupPage />} />
-//       {userRole === "organizier" && (
-//         <Route path="/dashboard" element={<OrganizerDashboard />} />
-//       )}
-//       {userRole === "customer" && (
-//         <Route path="/dashboard" element={<CustomerDashboard />} />
-//       )}
-//     </Routes>
-//   );
-// };
-
-// export default AllRoutes;
