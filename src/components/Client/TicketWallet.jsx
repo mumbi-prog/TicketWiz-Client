a1a726048e08ba0bc2445e36bdef8169bbe6797b
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-30 04:45:46 +0300

    added loading state

--- src/components/Client/TicketWallet.jsx
+++ src/components/Client/TicketWallet.jsx
@@ -1,9 +1,22 @@
 import React, { useEffect, useState } from 'react';
+import './LoaderStyling.css';
 
 function TicketWallet() {
   const [ticketWallet, setTicketWallet] = useState([]);
   const [loading, setLoading] = useState(true);
-  
+
+  if (loading) {
+    return (
+      <section className="dots-container mt-[15%]">
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+      </section>
+    );
+  }
+
   return (
     <div>
         <h1>TicketWallet</h1>
