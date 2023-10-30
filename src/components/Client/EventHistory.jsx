53f50a32b9c49b8422b8e0574ab15abaa36f6809
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-30 04:05:51 +0300

    Added states to fetch and display data

--- src/components/Client/EventHistory.jsx
+++ src/components/Client/EventHistory.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { FaLocationDot } from 'react-icons/fa';
 
 function EventHistory() {
+
+  const [eventHistory, setEventHistory] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   return (
     <div>
         <h1>EventHistory</h1>
