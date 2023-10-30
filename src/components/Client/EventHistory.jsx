ce591f64498e3ec211811789c6cea6e5da56b0dd
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-30 04:06:38 +0300

    added format date function

--- src/components/Client/EventHistory.jsx
+++ src/components/Client/EventHistory.jsx
@@ -1,6 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { FaLocationDot } from 'react-icons/fa';
 
+function formatDate(dateString) {
+    const date = new Date(dateString);
+    const month = date.toLocaleDateString(undefined, { month: 'short' });
+    const day = date.getDate();
+    return (
+      <div className="event-date font- font-bold flex flex-col items-center justify-center mt-[2px]">
+        <p className="month text-main-blue font-medium text-sm">{month}</p>
+        <p className="day text-3xl mt-[-2px] font-bold">{day}</p>
+      </div>
+    );
+  }
+
 function EventHistory() {
 
   const [eventHistory, setEventHistory] = useState([]);
