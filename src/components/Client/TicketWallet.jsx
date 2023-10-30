66c4d8d7b0b99554b4f8f2c275190d71c3dfef20
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-30 04:44:18 +0300

    added ticket wallet and loadinfg states

--- src/components/Client/TicketWallet.jsx
+++ src/components/Client/TicketWallet.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 function TicketWallet() {
+  const [ticketWallet, setTicketWallet] = useState([]);
+  const [loading, setLoading] = useState(true);
+  
   return (
     <div>
         <h1>TicketWallet</h1>
