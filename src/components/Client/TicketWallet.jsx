8f258e98b3140d0c687be1efe4b16485a434c1fe
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-30 04:46:57 +0300

    fetch ticket wallet

--- src/components/Client/TicketWallet.jsx
+++ src/components/Client/TicketWallet.jsx
@@ -5,6 +5,18 @@ function TicketWallet() {
   const [ticketWallet, setTicketWallet] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    fetch('http://127.0.0.1:3000/ticket_wallet')
+      .then((response) => response.json())
+      .then((data) => {
+        setTicketWallet(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching ticket wallet:', error);
+      });
+  }, []);
+
   if (loading) {
     return (
       <section className="dots-container mt-[15%]">
