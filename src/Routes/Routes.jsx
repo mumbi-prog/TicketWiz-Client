c4375f7f779b757f389ab46685b224b3ed36f9f0
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-31 16:43:39 +0300

    Updated org login

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -6,20 +6,21 @@ import OrganizerSignupPage from "../components/Login/organiserSignup";
 import CustomerSignupPage from '../components/Login/signup';
 import CustomerLoginPage from "../components/Login/customerLogin";
 import HomePage from '../Pages./Home';
-import OrganizerDashboard from "../components/Organizer/Dashboard";
+// import OrganizerDashboard from "../components/Organizer/Dashboard";
 // import CustomerDashboard from "../components/Client/Dashboard";
 import ProfilePage from "../components/Client/Profile";
+import OrganizerProfile from "../components/Organizer/OrganizerProfile";
 
 const AllRoutes = () => {
   const [ setUserRole] = useState(""); 
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/home" element={<HomePage />} />
       <Route path="/customersignup" element={<CustomerSignupPage />} />
       <Route path="/customerlogin" element={<CustomerLoginPage/>} />
       <Route path="/organiserlogin" element={<LoginPage setUserRole={setUserRole} />} />
       <Route path="/organisersignup" element={<OrganizerSignupPage />} />
-      <Route path="/organizerdashboard" element={<OrganizerDashboard />} />
+      <Route path="/organizerdashboard" element={<OrganizerProfile />} />
       <Route path="/customerdashboard" element={<ProfilePage />} />
      
     </Routes>
