495cc6d8c9686efa216e608922223d4426aaccf1
Sylvia Mumbi <mumbi099@gmail.com>
2023-11-02 11:22:04 +0300

    Updated and tested the update functionality

--- src/components/Organizer/Dashboard.jsx
+++ src/components/Organizer/Dashboard.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import Modal from './UpdateModal';
-import { BiSolidEditAlt } from 'react-icons/bi';
-import { BsTrash3 } from 'react-icons/bs'
-import { FaLocationDot } from 'react-icons/fa6';
-import api from '../api/Api';
+import UpdateModal from './UpdateModal';
 import DeleteConfirmationModal from './DeleteConfirmationModal';
+import api from '../api/Api';
+import { BiSolidEditAlt } from 'react-icons/bi';
+import { BsTrash3 } from 'react-icons/bs';
+import { FaLocationDot } from 'react-icons/fa6';
 
 function formatDate(dateString) {
   const date = new Date(dateString);
@@ -23,32 +23,17 @@ function Dashboard() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const [eventCount, setEventCount] = useState(0);
-
-  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
-  const [eventToDelete, setEventToDelete] = useState(null);
-
   const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(false);
   const [eventToUpdate, setEventToUpdate] = useState(null);
 
-  const [updatedEventTitle, setUpdatedEventTitle] = useState('');
-  const [updatedEventDesc, setUpdatedEventDesc] = useState('');
-  const [updatedEventCategory, setUpdatedEventCategory] = useState('');
-  const [updatedEventPoster, setUpdatedEventPoster] = useState('');
-  const [updatedEventDate, setUpdatedEventDate] = useState('');
-  const [updatedEventStartTime, setUpdatedEventStartTime] = useState('');
-  const [updatedEventEndTime, setUpdatedEventEndTime] = useState('');
-  const [updatedEventVenue, setUpdatedEventVenue] = useState('');
-  const [updatedEventLocation, setUpdatedEventLocation] = useState('');
-  const [updatedEventPrice, setUpdatedEventPrice] = useState('');
-  const [updatedEventTicketCount, setUpdatedEventTicketCount] = useState('');
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [eventToDelete, setEventToDelete] = useState(null);
 
   useEffect(() => {
     api.get('/organiser_dashboard')
       .then((response) => response.data)
       .then((data) => {
         setOrganizerData(data);
-        setEventCount(data.length);
         setLoading(false);
       })
       .catch((error) => {
@@ -59,17 +44,6 @@ function Dashboard() {
   const openUpdateModal = (event) => {
     setIsUpdateModalOpen(true);
     setEventToUpdate(event);
-    setUpdatedEventTitle(event.title);
-    setUpdatedEventDesc(event.description);
-    setUpdatedEventCategory(event.category);
-    setUpdatedEventPoster(event.image_url);
-    setUpdatedEventDate(event.date);
-    setUpdatedEventStartTime(event.start_time);
-    setUpdatedEventEndTime(event.end_time);
-    setUpdatedEventVenue(event.venue_name);
-    setUpdatedEventLocation(event.event_location);
-    setUpdatedEventPrice(event.price);
-    setUpdatedEventTicketCount(event.available_tickets_count);
   };
 
   const closeUpdateModal = () => {
@@ -87,15 +61,36 @@ function Dashboard() {
     setEventToDelete(null);
   };
 
+  const handleUpdateEvent = (updatedEventData) => {
+    api
+      .patch(`/events/${eventToUpdate.id}`, updatedEventData)
+      .then((response) => {
+        if (response.status === 200) {
+          setOrganizerData((prevEvents) =>
+            prevEvents.map((event) =>
+              event.id === eventToUpdate.id ? { ...event, ...updatedEventData } : event
+            )
+          );
+          closeUpdateModal();
+          
+        } else {
+          console.error('Failed to update event');
+        }
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  };
+  
+
   const confirmDeleteEvent = (event) => {
     closeDeleteModal();
-  
+
     api
       .delete(`/events/${event.id}`)
       .then((response) => {
         if (response.status === 204) {
           setOrganizerData((prevEvents) => prevEvents.filter((e) => e.id !== event.id));
-          setEventCount((prevCount) => prevCount - 1);
         } else {
           console.error('Failed to delete event');
         }
@@ -104,7 +99,6 @@ function Dashboard() {
         console.error('Error while deleting event:', err);
       });
   };
-  
 
   if (loading) {
     return (
@@ -118,55 +112,10 @@ function Dashboard() {
     );
   }
 
-  const handleUpdateEvent = () => {
-    const updatedEventData = {
-      title: updatedEventTitle,
-      description: updatedEventDesc,
-      category: updatedEventCategory,
-      image_url: updatedEventPoster,
-      date: updatedEventDate,
-      start_time: updatedEventStartTime,
-      end_time: updatedEventEndTime,
-      venue_name: updatedEventVenue,
-      event_location: updatedEventLocation,
-      price: updatedEventPrice,
-      available_tickets_count: updatedEventTicketCount,
-    };
-
-    api
-      .patch(`/events/${eventToUpdate.id}`, updatedEventData)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Cannot update');
-        }
-        return response.json();
-      })
-      .then((updatedEvent) => {
-        setOrganizerData((prevEvents) =>
-          prevEvents.map((event) =>
-            event.id === updatedEvent.id ? updatedEvent : event
-          )
-        );
-        closeUpdateModal();
-      })
-      .catch((err) => {
-        setError(err);
-      });
-  };
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
   return (
     <div className="organizer-dashboard m-[20px]">
       <div className="dashboard-container">
         <h2 className="dashboard-title">Organizer Dashboard</h2>
-
-        <div className="stats-container bg-gray-500 px-[30px] py-[30px] font-bold inline-block mt-[20px] text-lg rounded-md transition-transform transform hover:scale-105 hover:bg-acc-color">
-          <p className="event-count font-bold">Total Events</p>
-          <p className='font-bold'>{eventCount}</p>
-        </div>
 
         <div className="event-list grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 my-12">
           {organizerData.map((event) => (
@@ -176,7 +125,7 @@ function Dashboard() {
               </div>
               <div className="event-selected-details flex p-5">
                 <div className="event-date">
-                  <p>{formatDate(event.date)}</p>
+                  {formatDate(event.date)}
                 </div>
                 <div className="event-venue pl-6">
                   <h3 className="event-title font-sans mt-0 text-lg font-bold uppercase text-black ml-2">
@@ -201,39 +150,13 @@ function Dashboard() {
         </div>
       </div>
       {isUpdateModalOpen && (
-        <Modal onClose={closeUpdateModal}>
-          <h3>Edit Event</h3>
-          <input type="text" placeholder="Event Title" value={updatedEventTitle} onChange={(e) => setUpdatedEventTitle(e.target.value)} />
-          <input type="text" placeholder="Event Description" value={updatedEventDesc} onChange={(e) => setUpdatedEventDesc(e.target.value)} />
-          <input type="text" placeholder="Event Category" value={updatedEventCategory} onChange={(e) => setUpdatedEventCategory(e.target.value)} />
-          <input type="text" placeholder="Event Poster" value={updatedEventPoster} onChange={(e) => setUpdatedEventPoster(e.target.value)} />
-          <input type="text" placeholder="Event Date" value={updatedEventDate} onChange={(e) => setUpdatedEventDate(e.target.value)} />
-          <input type="text" placeholder="Event Start Time" value={updatedEventStartTime} onChange={(e) => setUpdatedEventStartTime(e.target.value)} />
-          <input type="text" placeholder="Event End Time" value={updatedEventEndTime} onChange={(e) => setUpdatedEventEndTime(e.target.value)} />
-          <input type="text" placeholder="Event Venue Name" value={updatedEventVenue} onChange={(e) => setUpdatedEventVenue(e.target.value)} />
-          <input type="text" placeholder="Event Location" value={updatedEventLocation} onChange={(e) => setUpdatedEventLocation(e.target.value)} />
-          <input type="text" placeholder="Event Price" value={updatedEventPrice} onChange={(e) => setUpdatedEventPrice(e.target.value)} />
-          <input type="text" placeholder="Event Ticket Count" value={updatedEventTicketCount} onChange={(e) => setUpdatedEventTicketCount(e.target.value)} />
-          <div className="confirm-buttons flex flex-row gap-15">
-            <button onClick={handleUpdateEvent} className="bg-main-blue text-white px-4 py-2 rounded">
-              Update
-            </button>
-            <button onClick={closeUpdateModal} className="bg-gray-300 text-gray-700 px-4 py-2 rounded">
-              Cancel
-            </button>
-          </div>
-        </Modal>
+        <UpdateModal onClose={closeUpdateModal} onUpdate={handleUpdateEvent} eventData={eventToUpdate} />
       )}
-
       {isDeleteModalOpen && (
-        <DeleteConfirmationModal 
-          onCancel={closeDeleteModal}
-          onConfirm={() => confirmDeleteEvent(eventToDelete)}
-        />
+        <DeleteConfirmationModal onCancel={closeDeleteModal} onConfirm={() => confirmDeleteEvent(eventToDelete)} />
       )}
     </div>
   );
 }
 
 export default Dashboard;
-
--- src/components/Organizer/UpdateModal.jsx
+++ src/components/Organizer/UpdateModal.jsx
@@ -1,16 +1,216 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Modal({ onClose, children }) {
+function UpdateModal({ onClose, onUpdate, eventData }) {
+  const [formData, setFormData] = useState(eventData);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onUpdate(formData);
+    onClose();
+  };
+
+  const CATEGORIES = [
+    'Conferences', 'Workshops and Seminars', 'Networking Events', 'Trade Shows', 'Concerts', 'Sporting Events',
+    'Art Shows', 'Feast Event', 'Performing Arts', 'Charity Events', 'Parties', 'Educational Events',
+    'Tech and Hackathons', 'Adventure', 'Cultural', 'Virtual', 'Cosplay', 'Gaming',
+  ];
+
+  const handleCategoryChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
   return (
-    <div className="modal">
-      <div className="modal-content">
-        <button className="modal-close-button" onClick={onClose}>
-          Close
-        </button>
-        {children}
+    <div className="modal-overlay fixed top-0 left-0 w-full h-full bg-black bg-opacity-75 flex justify-center items-center z-50">
+      <div className="modal-content bg-white border border-gray-300 rounded-md px-5 w-[615px] shadow-md text-left py-[10px]">
+        <form onSubmit={handleSubmit}>
+          <div className='flex gap-[30px]'>
+            <div className="e-title">
+              <label className="block text-gray-600 text-sm font-bold mb-2" htmlFor="title">
+                Event Title:
+              </label>
+              <input
+                type="text"
+                name="title"
+                value={formData.title}
+                onChange={handleInputChange}
+                className="e-title p-[5px] w-[270px] border rounded-md bg-gray-200"
+                required
+              />
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-gray-600 text-sm font-bold mb-2" htmlFor="category">
+                Event Category:
+              </label>
+              <select
+                name="category"
+                value={formData.category}
+                onChange={handleCategoryChange}
+                className="e-category p-[5px] w-[270px] border rounded-md bg-gray-200"
+                required
+              >
+                <option value="">Select a category</option>
+                {CATEGORIES.map((category) => (
+                  <option key={category} value={category}>
+                    {category}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+
+          <div className='flex gap-[30px]'>
+            <div className="mb-2">
+              <label className="block text-gray-600 text-sm font-bold mb-2" htmlFor="venue_name">
+                Event Venue:
+              </label>
+              <input
+                type="text"
+                name="venue_name"
+                value={formData.venue_name}
+                onChange={handleInputChange}
+                className="e-venue p-[5px] w-[270px] border rounded-md bg-gray-200"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-600 text-sm font-bold mb-2" htmlFor="event_location">
+                Event Location:
+              </label>
+              <input
+                type="text"
+                name="event_location"
+                value={formData.event_location}
+                onChange={handleInputChange}
+                className="e-location p-[5px] w-[270px] border rounded-md bg-gray-200"
+                required
+              />
+            </div>
+          </div>
+
+          <div className="mb-4 mt-[-10px]">
+            <label className="block text-gray-600 text-sm font-bold mb-2" htmlFor="image_url">
+              Event Poster:
+            </label>
+            <input
+              type="text"
+              name="image_url"
+              value={formData.image_url}
+              onChange={handleInputChange}
+              className="e-poster p-[5px] w-[570px] border rounded-md bg-gray-200"
+              required
+            />
+          </div>
+
+          <div className='flex gap-[30px]'>
+            <div className="mb-4">
+              <label className="block text-gray-600 text-sm font-bold mb-2" htmlFor="date">
+                Date:
+              </label>
+              <input
+                type="date"
+                name="date"
+                value={formData.date}
+                onChange={handleInputChange}
+                className="e-date p-2 w-[150px] border rounded-md bg-gray-200"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-600 text-sm font-bold mb-2" htmlFor="start_time">
+                Start Time:
+              </label>
+              <input
+                type="time"
+                name="start_time"
+                value={formData.start_time}
+                onChange={handleInputChange}
+                className="e-start-time p-2 w-[150px] border rounded-md bg-gray-200"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-600 text-sm font-bold mb-2" htmlFor="end_time">
+                End Time:
+              </label>
+              <input
+                type="time"
+                name="end_time"
+                value={formData.end_time}
+                onChange={handleInputChange}
+                className="e-end-time p-2 w-[150px] border rounded-md bg-gray-200"
+                required
+              />
+            </div>
+          </div>
+
+          <div className="mb-4 w-[1030px]">
+            <label className="block text-gray-600 text-sm font-bold mb-2" htmlFor="description">
+              Description:
+            </label>
+            <textarea
+              name="description"
+              value={formData.description}
+              onChange={handleInputChange}
+              className="border rounded p-2 w-[570px] border rounded-md bg-gray-200"
+              required
+            />
+          </div>
+
+          <div className='flex gap-[30px]'>
+            <div>
+              <label className="block text-gray-600 text-sm font-bold mb-2" htmlFor="available_tickets_count">
+                Price:
+              </label>
+              <input
+                type="number"  
+                name="price"
+                value={formData.price}
+                onChange={handleInputChange}
+                className="e.price p-2 w-[270px] border rounded-md bg-gray-200"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-600 text-sm font-bold mb-2" htmlFor="available_tickets_count">
+                Available Tickets Count:
+              </label>
+              <input
+                type="number"
+                name="available_tickets_count"
+                value={formData.available_tickets_count}
+                onChange={handleInputChange}
+                className="e.ticket-count p-2 w-[270px] border rounded-md bg-gray-200"
+                required
+              />
+            </div>
+          </div>
+
+          <div className="confirm-buttons flex flex-row justify-center gap-[50px]">
+            <button type="submit" className="bg-main-blue text-white px-4 py-2 rounded px-[40px]">
+              Update
+            </button>
+            <button onClick={onClose} className="bg-gray-300 text-gray-700 px-4 py-2 rounded px-[40px]">
+              Cancel
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
 }
 
-export default Modal;
+export default UpdateModal;
+
