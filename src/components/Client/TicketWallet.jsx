11f1de785d026682609a34c331776828aeecb818
Sylvia Mumbi <mumbi099@gmail.com>
2023-11-03 15:01:29 +0300

    Update on ticket wallet

--- src/components/Client/TicketWallet.jsx
+++ src/components/Client/TicketWallet.jsx
@@ -34,18 +34,18 @@ function TicketWallet() {
 
   return (
     <div className="ticket-wallet mx-[20px] font-sans my-[30px]">
-      <h2 className='text-2xl font-bold mb-[10px] text-left mx-[50px]'>My Ticket Wallet</h2>
+      <h2 className='font-sans text-2xl font-medium mb-[15px]'>My Ticket Wallet</h2>
 
-      <div className="ticket-wallet-table-container rounded-md bg-gray-100 py-[20px] px-[50px] mx-[50px] inline-block mt-[25px]">
-          <p className='mb-[10px] text-left text-acc-blue font-medium text-lg'>Total Tickets: {totalTickets}</p>
+      <div className="ticket-wallet-table-container rounded-md bg-gray-100 py-[20px] px-[50px] inline-block mt-[25px]">
+          <p className='mb-[15px] text-left text-acc-blue font-medium text-lg'>Total Tickets : {totalTickets}</p>
           <div className="ticket-wallet-table ">
             <table className="">
             <thead>
               <tr>
-                <th className="w-1/4 py-2 px-10 bg-gray-500 text-white text-center border-r-[10px]">Event Name</th>
-                <th className="w-1/4 py-2 px-10 bg-gray-500 text-white text-center border-r-[10px]">Event Category</th>
-                <th className="w-1/4 py-2 px-10 bg-gray-500 text-white text-center border-r-[10px]">Ticket Type</th>
-                <th className="w-1/4 py-2 px-10 bg-gray-500 text-white text-center">Quantity</th>
+                <th className="w-[200px]  py-[10px] px-[20px] bg-gray-300 text-black font-medium text-center border-r-[20px]">Event name</th>
+                <th className="w-[230px]   py-[10px] px-[20px] bg-gray-300 text-black font-medium text-center border-r-[20px]">Event category</th>
+                <th className="w-1/4   py-[10px] px-[20px] bg-gray-300 text-black font-medium text-center border-r-[20px]">Ticket Type</th>
+                <th className="w-1/4  py-[10px] px-[20px] bg-gray-300 text-black font-medium text-center">Quantity</th>
               </tr>
             </thead>
               <tbody>
