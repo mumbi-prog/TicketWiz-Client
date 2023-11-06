4320be781680143b70d6de72bbd08d45232fcdc6
Sylvia Mumbi <mumbi099@gmail.com>
2023-11-06 05:01:36 +0300

    Fetch ticket sales info

--- src/components/Organizer/Ticketsales.jsx
+++ src/components/Organizer/Ticketsales.jsx
@@ -6,6 +6,20 @@ function TicketSales() {
   const [ticketSales, setTicketSales] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    api.get('/remaining_tickets_info')
+      .then((response) => response.data)
+      .then((data) => {
+        setTicketSales(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching ticket sales info:', error);
+      });
+  }, []);
+
+
+
   return (
       <div>TicketSales</div>
   );
